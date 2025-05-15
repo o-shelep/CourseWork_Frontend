a589e7f453ed566e725cc93cbaecf904bdc0a1a7
o-shelep <shelep.olha@student.uzhnu.edu.ua>
2025-05-15 12:50:40 +0300

    feat(router): configure basic routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import CreateNewPage from "./pages/CreateNewPage/CreateNewPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/assignments" element={<DashboardPage />} />
+        <Route path="/create" element={<CreateNewPage />} />
+      </Routes>
+    </Router>
   );
 }
 
