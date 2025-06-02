87d7facb228da8141d0ed7952a4c09002e764cdf
o-shelep <shelep.olha@student.uzhnu.edu.ua>
2025-06-02 12:40:20 +0300

    fix: remove role check in myAssignmentsPage

--- src/pages/MyAssignmentsPage/MyAssignmentsPage.jsx
+++ src/pages/MyAssignmentsPage/MyAssignmentsPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import TaskCard from "../../components/TaskCard/TaskCard";
 import BannerImg from "../../assets/Banner.svg";
-import { useTasks } from "../../hooks/useTasks";
 import { useUserProfile } from "../../hooks/useUserProfile";
 import { formatSubject, truncateDescription } from "../../utils/textUtil";
 import "./MyAssignmentsPage.css";
@@ -14,22 +13,11 @@ function MyAssignmentsPage() {
     loading: userLoading,
     error: userError,
   } = useUserProfile(token);
-  const { tasks, error: tasksError } = useTasks();
 
   if (userLoading) return <p>Завантаження профілю...</p>;
   if (userError) return <p>Помилка: {userError}</p>;
 
-  const isPureStudent =
-    user &&
-    user.roles &&
-    user.roles.length === 1 &&
-    user.roles.includes("ROLE_STUDENT");
-
-  const tasksToShow = isPureStudent
-    ? user.submissions?.map((sub) => sub.task) || []
-    : tasks;
-
-  const error = isPureStudent ? userError : tasksError;
+  const tasksToShow = user.submissions?.map((sub) => sub.task) || [];
 
   return (
     <div className="dashboard-container">
@@ -39,7 +27,6 @@ function MyAssignmentsPage() {
 
       <div className="dashboard-container-content">
         <img src={BannerImg} alt="St. Mission Banner" />
-        {error && <p className="error-message">{error}</p>}
         {tasksToShow.length === 0 && <p>Немає доступних місій.</p>}
         {tasksToShow.map((task) => (
           <div key={task.id} className="task-card-container">
